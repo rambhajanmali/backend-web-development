73a266ebc79a59f959d52526cde540735f7f6eff
rambhajan mali <rambhajanmali987@gmail.com>
2024-03-29 01:08:43 +0530

    Added node js

---
+++ math.js
@@ -0,0 +1,23 @@
+// exports.sum = (a,b) => a + b;
+// exports.mul = (a , b) => a*b;
+// exports.g = 9.8;
+// exports.PI = 3.14;
+
+//OR
+
+// module.exports = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI,
+// };
+
+
+//using import
+
+export const sum = (a,b) => a + b;
+export const mul = (a , b) => a*b;
+export const g = 9.8;
+export const PI = 3.14;
+
+ 
---
+++ script.js
@@ -0,0 +1,16 @@
+
+// let args = process.argv;
+// for(i = 2; i<args.length; i++){
+//     console.log("hello to", args[i]);
+// }
+
+
+// const info = require("./fruits");
+// console.log(info);
+
+
+//using import 
+
+import { sum, g } from "./math.js";
+
+console.log(sum);
